ea0e4bff9df3911daf9f2bd81caa19dfaa76b9dc
Alex <alja.platt@gmail.com>
2022-10-12 17:01:13 +0100

    mvp complete

--- Game.py
+++ Game.py
@@ -1,6 +1,8 @@
 class Game:
+    # self gives you access to the current instance object
     # def __init__(self, winning_score):
-    def __init__(self):
+    def __init__(self, name):
+        self.player_name = name
         self.player_score = 0
         self.computer_score = 0
         #* ability to change winning score? 
@@ -10,7 +12,11 @@ class Game:
     def is_playing(self):
         if self.player_score != self.winning_score and self.computer_score != self.winning_score:
             return True
-    # def __str__():
+
+    # def print_score(self):
+        
+    def print_score(self):
+        return f"The score is {self.player_name}: {self.player_score}, Computer: {self.computer_score}"
 
     # Active player - simulate computer making a choice ?? 
     
@@ -20,4 +26,9 @@ class Game:
         else:
             self.computer_score += 1
     
-    # def print_final_score(self):
+    def print_winner(self):
+        if self.player_score == self.winning_score:
+            return f"{self.player_name}, you have won!"
+        else:
+            return f"Bad luck, the computer has won."
+    
--- main.py
+++ main.py
@@ -3,25 +3,30 @@ from Coin import Coin
 from Player import Player
 
 def main():
-    game = Game()
-    coin = Coin()
-    # print(coin.options)
-
-    # player 1  inherit from 
+    # player 1  inherit from comp class ??
     player_1 = Player(input("Please enter your name: "))
     Computer = Player('Computer')
-    # print(player_1.name)
+
+    game = Game(player_1.name)
+    coin = Coin()
+    # print(coin.options)
+    
     while game.is_playing():
         toss = coin.toss_coin()
-        print('COIN TOSS:', toss)
+        # print('COIN TOSS:', toss)
 
         player_choice = player_1.coin_choice()
         # print(player_choice)
 
         game.update_score(player_choice, toss)
 
-        print("Ply: ", game.player_score)
-        print('Com: ', game.computer_score)
+        # print("Ply: ", game.player_score)
+        # print('Com: ', game.computer_score)
+        print(game.print_score())
+    
+    print(game.print_winner())
+
+
 
 #? if this is a module, we only want to call it when we run main, not if it is imported
 # safety - only run main if you run the file main from the cli
