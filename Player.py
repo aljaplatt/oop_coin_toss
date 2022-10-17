daf5a06d1b22835316e69e8d8daf995ce58525f3
Alex <alja.platt@gmail.com>
2022-10-17 16:18:21 +0100

    added getter and setter for Player class

--- Player.py
+++ Player.py
@@ -4,12 +4,31 @@ class Player:
         if not name:
             raise ValueError("Missing name")
         self.name = name.title()
-        # self.score = 0
+    
+
+    @property
+    def name(self):
+    #? underscore = protected or private 
+        print("getting name...")
+        return self._name
+
+
+    @name.setter
+    def name(self, name):
+        print("setting name...")
+        self._name = name
+
 
     @classmethod
     def coin_choice(cls):
         # clear()
         return input('Guess the coin toss: ')
+    
+
+    def __str__(self):
+        return f"The players name is... {self.name}"
+    
+    __repr__ = __str__
 
 
 #* WILD CARD - WILL MAKE THE COMPUTER HAVE TO GUESS FROM THREE OPTIONS INSTEAD OF TWO 
